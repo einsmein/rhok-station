50ecbc936b76f8cac97dcda6dff1764c6d58c689
Antoni Benavent <ab@agrirobot.ai>
2022-05-01 14:12:16 +0200

    Integrated with data to db

--- data_to_db.py
+++ data_to_db.py
@@ -1,11 +1,14 @@
+from logging import exception
 import psycopg2
+from psycopg2 import IntegrityError
 import pandas as pd
 import io
+import os
 
 from requests import delete
 
 
-def get_csv_from_file(file_path: str)->pd.DataFrame:
+def get_csv_from_file(file_path)->pd.DataFrame:
 
     with open(file_path) as file:
         raw_csv = file.read()
@@ -21,31 +24,28 @@ def get_csv_from_file(file_path: str)->pd.DataFrame:
     return df
 
 
-def get_csv_data()->pd.DataFrame:
-
-    return get_csv_from_file("/home/antoni/hackaton/data/data_3.csv")
-
-
-def main():
+def insert_data(file_path):
     # Connect to database
-    conn = psycopg2.connect("host= localhost dbname= guest user= guest password= guest")
+    conn = psycopg2.connect(f'host= localhost dbname= postgres user= {os.getenv("POSTGRES_USER")} password= {os.getenv("POSTGRES_PASSWORD")}')
     cur = conn.cursor()
 
     tablename = "visits"
 
-    # # Build SQL code to drop table if exists and create table
-    # sqlQueryCreate = f'DROP TABLE IF EXISTS {tablename};\n CREATE TABLE {tablename} (Date DATE UNIQUE,\n Enters INTEGER);'
+    # Build SQL code to drop table if exists and create table
+    # sqlQueryCreate = f'DROP TABLE IF EXISTS {tablename};\n CREATE TABLE {tablename} (Date DATE,\n Enters INTEGER);'
     # cur.execute(sqlQueryCreate)
 
     # Get the visits data
-    df = get_csv_data()
+    df = get_csv_from_file(file_path)
 
     for index, row in df.iterrows():
         sqlQueryInsert = f'INSERT INTO {tablename}( Date, Enters) VALUES( TO_TIMESTAMP(\'{row["Start Time"]}\',\'YYYY-MM-DD\'), {row["Enters"]})'
         try:
             cur.execute(sqlQueryInsert)
         except:
-            print('[INFO] Duplicate instance.')
+            print(f'[INFO] Duplicate instance. {row}')
+            cur.close()
+            raise
 
     # Save changes and close connection
     conn.commit()
@@ -53,4 +53,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        insert_data("/home/antoni/hackaton/data/data_2.csv")
+    except IntegrityError as err:
+        pass
