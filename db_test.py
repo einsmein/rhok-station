c36fa4bd4509fae590792e3610497a3f82665968
Antoni Benavent <ab@agrirobot.ai>
2022-05-01 11:26:49 +0200

    Code cleaned and added duplicate check

--- db_test.py
+++ db_test.py
@@ -1,56 +1,12 @@
-import csv
 import psycopg2
-import os
-import glob
+import pandas as pd
+import io
+
+from requests import delete
 
 
-conn = psycopg2.connect("host= localhost dbname= guest user= guest password= guest")
-print("Connecting to Database")
+def get_csv_from_file(file_path: str)->pd.DataFrame:
 
-
-directory = "/home/antoni/hackaton/data/"
-
-for file in os.listdir(directory):
-
-    file_path = f"{directory}{file}"
-
-    # Create a table name
-    tablename = "visits";  # file.replace(".csv", "")
-    print(tablename)
-
-    # Open file
-    fileInput = open(file_path, "r")
-
-    # Extract first line of file
-    firstLine = fileInput.readline().strip()
-
-    # Split columns into an array [...]
-    columns = firstLine.split(",")
-
-    # Build SQL code to drop table if exists and create table
-    sqlQueryCreate = 'DROP TABLE IF EXISTS ' + tablename + ";\n"
-    sqlQueryCreate += 'CREATE TABLE ' + tablename + "(Start_Time DATE,\n Enters INTEGER,\n"
-
-    valid_columns = ['Start_Time', 'Enters']
-    #some loop or function according to your requiremennt
-    # Define columns for table
-    # for column in columns:
-    #     column = column.replace(" ", "_")
-    #     if column in valid_columns:
-    #         sqlQueryCreate += f"{column} VARCHAR(64),\n"
-
-    sqlQueryCreate = sqlQueryCreate[:-2]
-    sqlQueryCreate += ");"
-
-    print(sqlQueryCreate)
-    cur = conn.cursor()
-    cur.execute(sqlQueryCreate)
-    conn.commit()
-    cur.close()
-
-    import pandas as pd
-    import io
-    # Create a new .csv file which removes all the quotations around the rows and values
     with open(file_path) as file:
         raw_csv = file.read()
         new_csv = raw_csv.replace('\n\"', '\n').replace('\"\n', '\n').replace(', ', ',').replace('\"', '')
@@ -59,36 +15,42 @@ for file in os.listdir(directory):
     df = pd.read_csv(sio, sep=',')
     df['Start Time'] = pd.to_datetime(df['Start Time'], format='%Y-%m-%dT%H:%M:%S.000000Z')
 
+    # Select only the columns of interest
     df = df[['Start Time', 'Enters']]
 
-    df.to_csv('clean_data.csv')
+    return df
+
+
+def get_csv_data()->pd.DataFrame:
+
+    return get_csv_from_file("/home/antoni/hackaton/data/data_3.csv")
+
+
+def main():
+    # Connect to database
+    conn = psycopg2.connect("host= localhost dbname= guest user= guest password= guest")
+    cur = conn.cursor()
+
+    tablename = "visits"
+
+    # # Build SQL code to drop table if exists and create table
+    # sqlQueryCreate = f'DROP TABLE IF EXISTS {tablename};\n CREATE TABLE {tablename} (Start_Time DATE UNIQUE,\n Enters INTEGER);'
+    # cur.execute(sqlQueryCreate)
+
+    # Get the visits data
+    df = get_csv_data()
 
     for index, row in df.iterrows():
+        sqlQueryInsert = f'INSERT INTO {tablename}( Start_Time, Enters) VALUES( TO_TIMESTAMP(\'{row["Start Time"]}\',\'YYYY-MM-DD\'), {row["Enters"]})'
+        try:
+            cur.execute(sqlQueryInsert)
+        except:
+            print('[INFO] Duplicate instance.')
 
-        sqlQueryInsert = f'INSERT INTO {tablename}('
-        for column in columns:
-            column = column.replace(" ", "_")
-            if column in valid_columns:
-                sqlQueryInsert = f'{sqlQueryInsert} {column},'
+    # Save changes and close connection
+    conn.commit()
+    cur.close()
 
-        sqlQueryInsert = sqlQueryInsert[:-1]
-        sqlQueryInsert = f'{sqlQueryInsert}) VALUES('
 
-        for idx, v in enumerate(row):
-            v = str(v)
-            v = v.replace(" ", "_")
-            if idx == 0:
-
-                v = f'TO_TIMESTAMP(\'{v[:-9]}\',\'YYYY-MM-DD\')'
-
-            sqlQueryInsert = f'{sqlQueryInsert} {v},'
-
-        sqlQueryInsert = sqlQueryInsert[:-1]
-        sqlQueryInsert = f'{sqlQueryInsert})'
-
-        print(sqlQueryInsert)
-
-        cur = conn.cursor()
-        cur.execute(sqlQueryInsert)
-        conn.commit()
-        cur.close()
+if __name__ == "__main__":
+    main()
